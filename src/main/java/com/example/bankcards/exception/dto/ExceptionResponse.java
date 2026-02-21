b02adb8eba4aa3e79931a21ad12f91de8db61d8e
nick-long-int <gnidenkonn@yandex.ru>
2026-02-21 13:30:27 +0300

    Фикс ответа

--- src/main/java/com/example/bankcards/exception/dto/ExceptionResponse.java
+++ src/main/java/com/example/bankcards/exception/dto/ExceptionResponse.java
@@ -1,9 +1,17 @@
 package com.example.bankcards.exception.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Builder
 public class ExceptionResponse {
     private String message;
